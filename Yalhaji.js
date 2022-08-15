de11ffc9cc62f91a8f794e3f966573418610cc20
Ridwan <106625392+Young-Alhaji@users.noreply.github.com>
2022-08-15 11:42:45 -0700

    Add files via upload
    Latest commit

--- Yalhaji.js
+++ Yalhaji.js
@@ -20,7 +20,8 @@ let allClients=[]
 			surname:snam.value,
 			email:emai.value,
 			password:pass.value,
-			allCarts:[]
+			allCarts:[],
+			priceOfCurrentCart:[]
 		}
 			let email=emai.value
 			let password=pass.value
@@ -117,7 +118,7 @@ function signin(){
  	
  			admin=JSON.parse(localStorage.adminPage)
  			let eachProducts={
- 				itemImage:itemImag.value ,
+ 				itemImage:base64String,
  				itemName:itemNam.value,
  				itemPrice:parseInt(itemPric.value),
  				itemDetails:itemDetail.value
@@ -136,11 +137,12 @@ function signin(){
  				success.innerText=('Item added Successfully')
  			}
  		}
-
- 		function encodeImageFileAsURL(element) {
-  var file = element.files[0];
+ 				let base64String=''
+ 		function encodeImageFileAsURL() {
+  var file = document.querySelector('input[type=file]').files[0];
   var reader = new FileReader();
   reader.onloadend = function() {
+  	base64String=reader.result
     console.log('RESULT', reader.result)
   }
   reader.readAsDataURL(file);
@@ -156,7 +158,11 @@ viewproducts=()=>{
         </div>
     </div> `
 	})
+	if(allAddedItems.length==0){
+		disp.innerHTML=`You Currently do have any Products in the store.<br>Proceed to Add Products to include New products to the Store`
+	}
 	console.log(allAddedItems)
+	
 }
 
 const deleteProduct = (inde)=>{
@@ -182,12 +188,16 @@ allAddedItems=JSON.parse(localStorage.adminPage).addedProducts
         </div>
     </div> `
     console.log(allClients[i])
-	})		
+	})	
+	currentClientCarts=JSON.parse(localStorage.allOfClients)[i].allCarts
+	currentClientCarts.map((item,i)=>
+			cartNo.innerHTML= currentClientCarts.length)	
 }
 
 const details=(deindex)=>{
-	localStorage.detailsindex=JSON.stringify(deindex)
-	location.assign('detailspage.html')
+	console.log(deindex)
+	localStorage.detailsindex=deindex
+	 location.assign('detailspage.html')
 }
 
 
@@ -197,51 +207,91 @@ const detailspage=()=>{
 
 currentClientCarts=JSON.parse(localStorage.allOfClients)[i].allCarts
  			if(currentClientCarts){
- 			 allCarts=currentClientContact
+ 			 allCarts=currentClientCarts
  			}
 
  	allClients=JSON.parse(localStorage.allOfClients)
 allAddedItems=JSON.parse(localStorage.adminPage).addedProducts
 deindex=JSON.parse(localStorage.detailsindex)
- let filteredDetails =allAddedItems.filter((item,i)=>deindex==i)
+ let filteredDetails =allAddedItems.find((item,i)=>deindex==i)
  allAddedItems = filteredDetails
+ console.log(filteredDetails,i)
 disp.innerHTML=`<div class="display">
 <div class='detailtext'>Details</div>
         <div>
-            <img class="img automargin" src=${allAddedItems[i].itemImage}></div>  <div class='detailtext'>${allAddedItems[i].itemName}</div> $${allAddedItems[i].itemPrice}<br>
-        ${allAddedItems[i].itemDetails} <br>
-        <button class="btn btn-primary w-50 mainbackcolor automargin" onclick="addToCart(${i})">Add to Cart</button> 
+            <img class="img automargin" src=${filteredDetails.itemImage}></div>  <div class='detailtext'>${filteredDetails.itemName}</div>
+             $${filteredDetails.itemPrice}<br>
+        ${filteredDetails.itemDetails} <br>
+        <button class="btn btn-primary w-50 mainbackcolor automargin" onclick="addToCart(${deindex})">Add to Cart</button> 
         <button class="btn btn-primary w-50 mainbackcolor automargin" onclick="back()">Back to Products</button><br><br>
     </div> `
+    console.log(i)
 }
 const back=()=>{
-	location.reload()
+	window.history.back()
 }
 
 const addToCart=(cartindex)=>{
  	allAddedItems=JSON.parse(localStorage.adminPage).addedProducts
- 	allCarts =allAddedItems.filter((item,ind)=>cartindex==ind)
+ 	let filteredCarts =allAddedItems.find((item,ind)=>cartindex==ind)
+ 	filteredCarts.quantity=parseInt(1)
+ 	allCarts=filteredCarts
  	i=JSON.parse(localStorage.indexes)
 	allClients=JSON.parse(localStorage.allOfClients)
+	console.log(allCarts)
  	allClients[i].allCarts.push(allCarts)
  	localStorage.allOfClients=JSON.stringify(allClients)
  	console.log(allClients[i].allCarts)
+ 	success.innerHTML='Item added to Cart'
+ 	
+ 	currentClientCarts=JSON.parse(localStorage.allOfClients)[i].allCarts
+	currentClientCarts.map((item,i)=>
+			cartNo.innerHTML= currentClientCarts.length)
 }
 
 
 cartpage=()=>{
+	let total=0;
 	i=JSON.parse(localStorage.indexes)
 	allClients=JSON.parse(localStorage.allOfClients)
 	currentClientCarts=JSON.parse(localStorage.allOfClients)[i].allCarts
 	currentClientCarts.map((item,i)=>{
 			disp.innerHTML+=`<div class="display">
         <div class="card">
-            <img class="img" src=${currentClientCarts[i].itemImage}>${currentClientCarts[i].itemName} <br> $${currentClientCarts[i].itemPrice}
-            <input type="" name="" id="itemQuantity" placeholder="Quantity" style="width: 50%;">
- 			<button onclick="updateQuantity()" id="upBtn">Update</button>
-            <button class="btn btn-primary w-100 mainbackcolor" onclick="deleteCart(${i})">---</button>
+            <img class="img" src=${currentClientCarts[i].itemImage}>${currentClientCarts[i].itemName} <br>
+             $${currentClientCarts[i].itemPrice}
+            <input type="number" name="" id="itemQuantit" placeholder="Quantity" value="1" style="width: 20%;
+             border:2px solid deepskyblue;" onkeypress="changeQuantity(${i})">
+            <button class="btn btn-primary w-100 mainbackcolor" onclick="deleteCart(${i})">Remove</button>
         </div>
     </div> `
-
-	})		
+    total += (currentClientCarts[i].itemPrice*currentClientCarts[i].quantity);
+    console.log()
+    totalPrice.innerHTML=`Total Price = $${total} `
+	})
+	if (currentClientCarts.length>0){
+		checkout.innerHTML= `<button class="btn btn-primary w-25 mainbackcolor" onclick="checkout(${i})">Checkout</button>`
+	}else{
+		checkout.innerHTML=`You Currently have no items in your Cart<br><br><br><br<br>
+							<button class="btn btn-primary w-50 mainbackcolor automargin" onclick="back()">
+							Back to Home</button><br>
+							<br><br<br><br><br<br><br><br<br><br><br`
+	}	
+	console.log(currentClientCarts)
+	currentClientCarts=JSON.parse(localStorage.allOfClients)[i].allCarts
+	currentClientCarts.map((item,i)=>
+			cartNo.innerHTML= currentClientCarts.length)
 }
+
+const deleteCart = (inde)=>{
+	currentClientCarts=JSON.parse(localStorage.allOfClients)[i].allCarts
+    let filteredArray =currentClientCarts.filter((item,ind)=>inde!=ind)
+    currentClientCarts = filteredArray
+    allClients=JSON.parse(localStorage.allOfClients)
+    allClients[i].allCarts= currentClientCarts
+    localStorage.allOfClients=JSON.stringify(allClients)
+    location.reload()
+}
+
+
+
